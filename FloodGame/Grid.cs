4ccf6d4479e8a3511caa91c50605243dfd56de58
Tadeáš Pikl <tadeas.pikl.021@pslib.cz>
2023-05-10 09:43:03 +0200

    c# stuff

---
+++ FloodGame/Grid.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloodGame
+{
+    internal class Grid
+    {
+        public GridTile[,] Tiles;
+        private Random rand = new Random();
+
+        public Grid(int width, int height, int colors)
+        {
+            Tiles = GenerateGrid(width, height, colors);
+        }
+
+        public Grid(int width, int colors)
+        {
+            Tiles = GenerateGrid(width, width, colors);
+        }
+
+
+        private GridTile[,] GenerateGrid(int width, int height, int colors)
+        {
+            GridTile[,] tiles = new GridTile[width, height];
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    tiles[i,j] = new GridTile(i,j,rand.Next(colors));
+                }
+            }
+        }
+    }
+}
---
+++ FloodGame/GridTile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloodGame
+{
+    internal class GridTile
+    {
+        public int x;
+        public int y;
+
+        public int colorId;
+
+        public GridTile(int x, int y, int colorId)
+        {
+            this.x = x;
+            this.y = y;
+            this.colorId = colorId;
+        }
+        
+    }
+}
