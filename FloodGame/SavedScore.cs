8a9c28d2123d03bfef92d889e92f97b346236bc3
TadeasPikl <tadeas+1@piklovi.cz>
2023-05-14 19:14:33 +0200

    published

---
+++ FloodGame/SavedScore.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloodGame
+{
+    internal class SavedScore
+    {
+        public string name;
+        public int moves;
+        public int width;
+        public int height;
+        public int colors;
+
+
+    }
+}
